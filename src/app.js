b136fa16cb48e5b80ffaebc9420ff1cb481369ad
Richard Smarsi <ricsmarsi@gmail.com>
2023-03-24 01:47:34 -0300

    Configurando middleware de erros globalmente no projeto para as rotas de API

--- src/app.js
+++ src/app.js
@@ -4,10 +4,15 @@ const router = require('./routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJson = require('../docs/swagger.json');
 
+const error_handler_middleware = require('./middlewares/error_handler_middleware');
+
 const app = express();
 
 app.use(express.json());
 app.use('/api', router);
+
+//Middlewares globais para API
+app.use(error_handler_middleware);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
