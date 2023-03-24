736446dd6892e1f6d150f07aff05785ac86e43d2
Richard Smarsi <ricsmarsi@gmail.com>
2023-03-24 01:57:10 -0300

    Adicionando needed permissions para alguns endpoints

--- src/routes/AdminRoutes.js
+++ src/routes/AdminRoutes.js
@@ -5,25 +5,38 @@ const router = express.Router();
 const AdminController = require('../controllers/AdminController');
 
 //Import Middlewares
-const onlyWhoCan_middleware = require('../middlewares/can');
 const validate_admin_middleware = require('../middlewares/validate_admin_middleware');
+const check_token_middleware = require('../middlewares/check_token_middleware');
+const onlyWhoCan_middleware = require('../middlewares/can');
 
 //Get all Admins accounts
-router.get('/listAccounts', AdminController.list);
+router.get('/listAccounts', 
+    check_token_middleware.checkTokenAndSetupPermissions,
+    onlyWhoCan_middleware.can(['list-admin']),
+    AdminController.list
+);
 
 //Create a new Admin
-router.post('/register', AdminController.register);
+router.post('/register',
+    check_token_middleware.checkTokenAndSetupPermissions,
+    onlyWhoCan_middleware.can(['create-admin']),
+    AdminController.register
+);
 
 //Login for admins
 router.post('/login', AdminController.login);
 
 //Get all permissions from a specific admin
 router.get('/:id_admin/permissions', 
+    check_token_middleware.checkTokenAndSetupPermissions,
+    onlyWhoCan_middleware.can(['list-permissions']),
     AdminController.getPermissionsByAdminId
 );
 
 //Relate new permissions for a admin
 router.post('/:id_admin/permissions',
+    check_token_middleware.checkTokenAndSetupPermissions,
+    onlyWhoCan_middleware.can(['give-permissions']),
     validate_admin_middleware.validateFieldsAndValuesOnAdminPermissions,
     validate_admin_middleware.checkPermissionsAndSetupRequest,
     AdminController.givePermissions
@@ -31,13 +44,19 @@ router.post('/:id_admin/permissions',
 
 //Remove permissions for a specific admin
 router.post('/:id_admin/remove-permissions',
+    check_token_middleware.checkTokenAndSetupPermissions,
+    onlyWhoCan_middleware.can(['give-permissions']),
     validate_admin_middleware.validateFieldsAndValuesOnAdminPermissions,
     validate_admin_middleware.checkPermissionsAndSetupRequest,
     AdminController.removePermissions
 );
 
 //Alter infos from a admin
-router.put('/update/:id_admin', AdminController.update);
+router.put('/update/:id_admin',
+    check_token_middleware.checkTokenAndSetupPermissions,
+    onlyWhoCan_middleware.can(['alter-admin']),
+    AdminController.update
+);
 
 
 module.exports = router;
--- src/routes/PermissionsRoutes.js
+++ src/routes/PermissionsRoutes.js
@@ -6,9 +6,13 @@ const PermissionsController = require('../controllers/PermissionsController');
 
 //Import Middlewares
 const validate_permissions_middleware = require('../middlewares/validate_permissions_middleware');
+const check_token_middleware = require('../middlewares/check_token_middleware');
+const onlyWhoCan_middleware = require('../middlewares/can');
 
 //Register a new permission
 router.post('/',
+      check_token_middleware.checkTokenAndSetupPermissions,
+      onlyWhoCan_middleware.can(['create-permissions']),
       validate_permissions_middleware.validateFieldsAndValuesOnPost,
       validate_permissions_middleware.checkIfAlreadyRegistred,
       PermissionsController.create
@@ -21,6 +25,8 @@ router.get('/',
 
 //Remove a specific permission
 router.delete('/:id_permission',
+      check_token_middleware.checkTokenAndSetupPermissions,
+      onlyWhoCan_middleware.can(['delete-permissions']),
       validate_permissions_middleware.checkIfIdExists,
       PermissionsController.deleteById
 );
