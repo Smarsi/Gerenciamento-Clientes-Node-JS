ce2e6a6dc2943217a82b324970bfb86a71a5877b
Richard Smarsi <ricsmarsi@gmail.com>
2023-03-30 12:35:35 -0300

    Alterando login de clientes para adequar ao padrão de middlewares.

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -5,17 +5,19 @@ const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 
 //Import Middlewares
+const validate_auth_middleware = require('../middlewares/validate_auth_middleware'); 
 
 //Default login
-router.post('/login', AuthController.login);
+router.post('/login',
+    validate_auth_middleware.validateFieldsAndValuesOnLogin,
+    validate_auth_middleware.checkEmailOnLogin,
+    AuthController.login
+);
 
 //Register new Account
 router.post('/register', AuthController.register);
 
 //Alter password for a Account
 router.post('/changePassword', AuthController.changePassword);
-
-//Teste check token
-router.get('/checkToken', AuthController.teste);
 
 module.exports = router;
